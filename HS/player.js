d73b16741a8509e75ca3bd53c71bef5988ea41fd
yalin1997 <godjj1997@gmail.com>
2018-06-20 18:03:22 +0800

    檢查是否死亡

--- HS/player.js
+++ HS/player.js
@@ -118,12 +118,15 @@ class player{
         let deadArr = [];
         for(i=0;i<this.playorder.length;i++){
             if(this.playorder[i].newDef<=0){
+                console.log(this.playorder[i].cardID+"死了");
                 deadArr.push(this.playorder[i]);
+                this.playorder.splice(i,1);
+                let tempindex =this.allayList.indexOf(this.playorder[i]);
+                this.allayList.splice(tempindex,1);
+                console.log(this.allayList);
+                
              }
         }
-        this.playorder.splice(i,1);
-        let tempindex =this.playersList.indexOf(this.playorder[i]);
-        this.allayList.splice(tempindex,1);
         return deadArr;
     }
     /*getdeck(mydeck){
