517b02e929a5b0a9a831646de5acee4b62ba1567
yalin1997 <godjj1997@gmail.com>
2018-06-21 02:50:16 +0800

    大便戰吼

--- HS/card/Poo.js
+++ HS/card/Poo.js
@@ -1,4 +1,6 @@
-﻿var card = require("./card")
+﻿var card = require("./card");
+var Poo1 = require("../skill/PooBattleCry");
+
 
 class Poo extends card {
     constructor(ID) {
@@ -6,11 +8,13 @@ class Poo extends card {
         this.name = "大便";
         this.race = "學長";
         this.cost = 8;
-        this.originAtk = 8;
-        this.originDef = 7;
+        this.originAtk = 6;
+        this.originDef = 5;
+        this.msg = "戰吼:友方手下改為3攻擊力，敵方手下改為2攻擊力。";
         this.cardType = "minion";
         this.newAtk = this.originAtk;
         this.newDef = this.originDef;
+        this.battlecayList.push(new Poo1());
     }
 }
 module.exports = Poo;
