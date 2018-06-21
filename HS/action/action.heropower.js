c4b9f5bf6aeacfb8df6afe717c4e0f188849db7f
yalin1997 <godjj1997@gmail.com>
2018-06-21 13:19:53 +0800

    訊息更新

--- HS/action/action.heropower.js
+++ HS/action/action.heropower.js
@@ -1,8 +1,8 @@
 var Action = require("./action");
 
 class heropower extends Action{
-    constructor(id,player,from,to){
-        super(id , Action.Type.Heropower , "英雄能力" , {} , player.id , from , to);
+    constructor(id,player,OBJ,from,to){
+        super(id , Action.Type.Heropower , "英雄能力" , OBJ , player.id , from , to);
     }
 }
 
